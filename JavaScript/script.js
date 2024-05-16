b0f901197f1251fe45c6f5c7a01ad6625d9707b7
juniom237 <junio237@hotmail.com>
2024-05-16 12:38:50 -0300

    atualizaçao

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+// Função para abrir o modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Função para fechar o modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
